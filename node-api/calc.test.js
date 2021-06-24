b98063392fa1dbb43b684797c507cfda1cebfedd
GabrielMM182 <gmm_morais@hotmail.com>
2021-06-24 09:21:05 -0300

    final-change

--- node-api/calc.test.js
+++ node-api/calc.test.js
@@ -1,5 +1,17 @@
 const calculator = require('./src/Calculator/calc');
 
-test('calcular soma ok' , () => {
-    expect(calculator.sum(2, 2).toBe(4));
+test('calculadora de soma deve estar ok', () => {
+    expect(calculator.sum(2, 2)).toBe(4);
+});
+
+test('calculadora de subtração deve estar ok', () => {
+    expect(calculator.menos(8, 4)).toBe(4);
+});
+
+test('calculadora de divisao deve estar ok', () => {
+    expect(calculator.div(16,4)).toBe(4);
+});
+
+test('calculadora de multiplicacao deve estar ok', () => {
+    expect(calculator.mult(2,2)).toBe(4);
 });
